ca7d12d1dee5a971ec7638ad7cdc4af9c06e7a92
frankayubi <frankayubi10@gmail.com>
2026-04-26 17:39:47 +0000

    words

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,36 +1,25 @@
+var i = 0;
+var ok = 0; // Start at 0 for typing
+var text1 = "Hey! Adeline.";
+var text2 = "Happy 1 weekiversary!";
 
-let i = 0;
-let text1 = "Hey! Adeline.";
-let text2 = "Happy 1 weekiversary !."
-let speed = 100;
-
-function typeWriter(text, para){
-	if(ok == 2){
-		clearInterval(typeInterval);
-	}
-	if(i < text.length){
-		document.getElementById(para).innerHTML += text.charAt(i);
-		i++;
-		speed = Math.random() * 50 + 100;
-	}
-	else{
-		if(ok == 0){
-			i = 0;
-		}
-		ok += 1;
-	}
+function typeWriter(text, para) {
+    if (i < text.length) {
+        document.getElementById(para).innerHTML += text.charAt(i);
+        i++;
+    } else {
+        i = 0;  // Reset character index for the next line
+        ok += 1; // Move to the next state
+    }
 }
 
-var typeInterval;
-
-//window.onload = function() {
-//	window.onload = function(){};
-   	typeInterval = setInterval(function(){
-		if(ok == 0){
-			typeWriter(text1, "txt1");
-		}
-		else if(ok == 1){
-			typeWriter(text2, "txt2");
-		}
-	}, 100);
-//};
+var typeInterval = setInterval(function() {
+    if (ok == 0) {
+        typeWriter(text1, "txt1");
+    } else if (ok == 1) {
+        typeWriter(text2, "txt2");
+    } else if (ok == 2) {
+        clearInterval(typeInterval);
+        // This is where your timer/fade-in script from earlier can take over!
+    }
+}, 100);
