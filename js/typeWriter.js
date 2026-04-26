b188c10f59af67ab7597fe61aa60705899807e69
frankayubi <frankayubi10@gmail.com>
2026-04-26 17:49:51 +0000

    words

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,31 +1,36 @@
-// Store all your messages in one list
-const messages = [
-    "Hey! Adeline",
-    "Happy 1 week .",
-    "You're sooooo cute!!!" // You can add as many as you want now!
-];
 
 let i = 0;
-let ok = 0; // The index of the message we are currently typing
+let text1 = "Hey! Adeline";
+let text2 = "Happy 1 weekaversery !."
+let speed = 100;
 
-function typeWriter() {
-    let currentText = messages[ok];
-    let elementId = "txt" + (ok + 1); // Looks for txt1, txt2, etc.
-
-    if (i < currentText.length) {
-        document.getElementById(elementId).innerHTML += currentText.charAt(i);
-        i++;
-    } else {
-        i = 0;
-        ok++; // Move to the next message index
-        
-        // Stop everything when we run out of messages
-        if (ok >= messages.length) {
-            clearInterval(typeInterval);
-            // Optional: Trigger your timer/fade-in here
-            startFadeIn(); 
-        }
-    }
+function typeWriter(text, para){
+	if(ok == 2){
+		clearInterval(typeInterval);
+	}
+	if(i < text.length){
+		document.getElementById(para).innerHTML += text.charAt(i);
+		i++;
+		speed = Math.random() * 50 + 100;
+	}
+	else{
+		if(ok == 0){
+			i = 0;
+		}
+		ok += 1;
+	}
 }
 
-var typeInterval = setInterval(typeWriter, 100);
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+   	typeInterval = setInterval(function(){
+		if(ok == 0){
+			typeWriter(text1, "txt1");
+		}
+		else if(ok == 1){
+			typeWriter(text2, "txt2");
+		}
+	}, 100);
+//};
