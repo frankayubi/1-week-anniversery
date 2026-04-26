45a308dd318821c9b0bbd33c1fd58c13e08c2e3f
frankayubi <frankayubi10@gmail.com>
2026-04-26 17:33:53 +0000

    words

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,7 +1,7 @@
 
 let i = 0;
-let text1 = "Hey! Miss Qiu.";
-let text2 = "Today is the Big Day."
+let text1 = "Hey! Adeline.";
+let text2 = "Happy 1 weekiversary !."
 let speed = 100;
 
 function typeWriter(text, para){
