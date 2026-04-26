c23fbebc7558795b37370c1fcfc6e1be317fd500
frankayubi <frankayubi10@gmail.com>
2026-04-26 18:24:38 +0000

    words

--- js/playImg.js
+++ js/playImg.js
@@ -1,74 +1,35 @@
-
 var btn = document.getElementById("heartTxt");
 btn.style.opacity = 0;
 var btnVal = 0;
 
-function showImage(){
-	//document.getElementById("imgTxt").style.opacity = 0;
-	myImage.setAttribute("src", imageArray[imageIndex]);
-	myTxt.innerHTML = txtArray[imageIndex];
-	//document.getElementById("imgTxt").style.opacity = 1 - flag;
-	imageIndex++;
-	if(imageIndex >= len){
-		imageIndex = 0;
-	}
+function play() {
+    // Toggles visibility: 1 - 1 = 0 (hides text), 1 - 0 = 1 (shows media)
+    flag = 1 - flag; 
+    document.getElementById("typeDiv").style.opacity = flag;
+    document.getElementById("imgTxt").style.opacity = 1 - flag;
 }
 
-function play(){
-	if(t == 0){
-		myImage.setAttribute("src", "");
-		myTxt.innerHTML = "";
-		imageIndex = 0;
-		clearInterval(showImageInterval);
-	}
-	flag = 1 - flag;
-	document.getElementById("typeDiv").style.opacity = flag;
-	document.getElementById("imgTxt").style.opacity = 1 - flag;
-	if(t == 0){
-		//setTimeout(showImage, 1000);
-		setInterval(showImage, 2500);
-	}
-	t++;
+function buttonFadeIn() {
+    if (btnVal < 1) {
+        btnVal += 0.025;
+        btn.style.opacity = btnVal;
+    } else {
+        clearInterval(buttonInterval);
+    }
 }
 
-function preshowImage(){
-	document.getElementById("imgTxt").style.opacity = 0;
-	myImage.setAttribute("src", imageArray[imageIndex]);
-	myTxt.innerHTML = txtArray[imageIndex];
-	imageIndex++;
-	if(imageIndex >= len){
-		imageIndex = 0;
-	}
+function event() {
+    // Monitors the typewriter progress (ok variable)
+    imgInterval = setInterval(function () {
+        if (ok == 3) {
+            setTimeout(function() {
+                buttonInterval = setInterval(buttonFadeIn, 50);
+            }, 1500);
+            clearInterval(imgInterval);
+        }
+    }, 50);
 }
 
-function buttonFadeIn(){
-	if(btnVal < 1){
-		btnVal += 0.025;
-		btn.style.opacity = btnVal;
-	}
-	else{
-		clearInterval(buttonInterval);
-		if(ok == 3){
-			ok += 1;
-		}
-	}
-}
-
-
-
-function event(){
-
-	showImageInterval = setInterval(preshowImage, 100);
-
-	imgInterval = setInterval(function (){
-		if(ok == 3){
-			setTimeout(function(){buttonInterval = setInterval(buttonFadeIn, 50);}, 1500);
-			clearInterval(imgInterval);
-		}
-	}, 50);
-}
-
-var showImageInterval;
 var imgInterval;
 var buttonInterval;
 
