767bbded3332f5eb9c38ef43764dc71ffe29bdb9
frankayubi <frankayubi10@gmail.com>
2026-04-26 17:42:22 +0000

    words

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,25 +1,31 @@
-var i = 0;
-var ok = 0; // Start at 0 for typing
-var text1 = "Hey! Adeline.";
-var text2 = "Happy 1 weekiversary!";
+// Store all your messages in one list
+const messages = [
+    "Hey! Adeline",
+    "Happy 1 week .",
+    "You're sooooo cute!!!" // You can add as many as you want now!
+];
 
-function typeWriter(text, para) {
-    if (i < text.length) {
-        document.getElementById(para).innerHTML += text.charAt(i);
+let i = 0;
+let ok = 0; // The index of the message we are currently typing
+
+function typeWriter() {
+    let currentText = messages[ok];
+    let elementId = "txt" + (ok + 1); // Looks for txt1, txt2, etc.
+
+    if (i < currentText.length) {
+        document.getElementById(elementId).innerHTML += currentText.charAt(i);
         i++;
     } else {
-        i = 0;  // Reset character index for the next line
-        ok += 1; // Move to the next state
+        i = 0;
+        ok++; // Move to the next message index
+        
+        // Stop everything when we run out of messages
+        if (ok >= messages.length) {
+            clearInterval(typeInterval);
+            // Optional: Trigger your timer/fade-in here
+            startFadeIn(); 
+        }
     }
 }
 
-var typeInterval = setInterval(function() {
-    if (ok == 0) {
-        typeWriter(text1, "txt1");
-    } else if (ok == 1) {
-        typeWriter(text2, "txt2");
-    } else if (ok == 2) {
-        clearInterval(typeInterval);
-        // This is where your timer/fade-in script from earlier can take over!
-    }
-}, 100);
+var typeInterval = setInterval(typeWriter, 100);
